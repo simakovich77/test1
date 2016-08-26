5f9a6818f5dee082f6b1b159e70cbec06e756180
Симакович Иван Алексеевич <sim.i.a@cnti.rw>
2016-08-26 16:13:30 +0300

    ot miasa1

--- public_html/protected/views/layouts/main.php
+++ public_html/protected/views/layouts/main.php
@@ -1,39 +1,3 @@
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="language" content="en">
-
-
-	<link rel="stylesheet" type="text/css" href=<?php echo Yii::app()->request->baseUrl; ?>"/css/bootstrap.min.css">
-
-	<link rel="stylesheet" type="text/css" href=<?php echo Yii::app()->request->baseUrl; ?>"/css/icomoon-social.css">
-
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600,800' rel='stylesheet' type='text/css'>
-
-	<link rel="stylesheet" type="text/css" href=<?php echo Yii::app()->request->baseUrl; ?>"/css/leaflet.css" />
-	<!--[if lte IE 8]>
-	<link rel="stylesheet" type="text/css" href=<?php echo Yii::app()->request->baseUrl; ?>"/css/leaflet.ie.css" />
-	<![endif]-->
-	<link rel="stylesheet" type="text/css" href=<?php echo Yii::app()->request->baseUrl; ?>"/css/main.css">
-
-	<title>Мое приложение<?php echo CHtml::encode($this->pageTitle); ?></title>
-	<meta>Мое приложение<?php echo $this->myVar; ?></meta>
-	
-</head>
-
-<body>
-
-<div class="container" id="page">
-
-	<div id="header">
-		<div id="logo">
-			<?php
-			echo CHtml::image('img/mPurpose-logo.png');
-			?>
-			</div>
-
 
 	<div id="mainmenu">
 		<?php //$this->widget('zii.widgets.CMenu',array(
