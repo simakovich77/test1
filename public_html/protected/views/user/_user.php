f26fab9185a15cc40b83ee14c849d3eaf2934cd7
sim.i.a <simakovich77@gmail.com>
2016-09-26 15:53:15 +0300

    dfgdfgdfgdfg3

--- public_html/protected/views/user/_user.php
+++ public_html/protected/views/user/_user.php
@@ -1,3 +1,217 @@
-<?php
-echo CHtml::image($data->img);
-?>
+<!-- Page Title -->
+<div class="section section-breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Our Team</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <!-- Team Member -->
+            <div class="col-md-4 col-sm-6">
+                <div class="team-member">
+                    <!-- Team Member Photo -->
+
+                    <div class="team-member-image"><img src="<?php
+
+                        echo $data->img; ?>"
+                                                        alt="Name Surname"></div>
+
+                    <div class="team-member-info">
+                        <ul>
+                            <!-- Team Member Info & Social Links -->
+                            <li class="team-member-name">
+                                Name Surname
+                                <!-- Team Member Social Links -->
+										<span class="team-member-social">
+											<a href="#"><i class="icon-facebook"></i></a>
+											<a href="#"><i class="icon-github"></i></a>
+											<a href="#"><i class="icon-tumblr"></i></a>
+										</span>
+                            </li>
+                            <li>Web Developer</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- End Team Member -->
+
+
+
+
+
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container">
+        <h2>Join Us!</h2>
+        <div class="row">
+            <!-- Open Vacancies List -->
+            <div class="col-md-8">
+                <table class="jobs-list">
+                    <tr>
+                        <th>Position</th>
+                        <th>Location</th>
+                        <th>Type</th>
+                    </tr>
+                    <tr>
+                        <!-- Position -->
+                        <td class="job-position">
+                            <a href="page-job-details.html">Front End Developer</a> <span class="label label-danger">New</span>
+                        </td>
+                        <!-- Location -->
+                        <td class="job-location">
+                            <div class="job-country">United Kingdom</div>
+                            <div class="job-city">London</div>
+                        </td>
+                        <!-- Job Type -->
+                        <td class="job-type hidden-phone">FULL-TIME</td>
+                    </tr>
+                    <tr>
+                        <td class="job-position">
+                            <a href="page-job-details.html">Back-end Developer</a> <span class="label label-danger">New</span>
+                        </td>
+                        <td class="job-location">
+                            <div class="job-country">United Kingdom</div>
+                            <div class="job-city">Manchester</div>
+                        </td>
+                        <td class="job-type hidden-phone">PART-TIME</td>
+                    </tr>
+                    <tr>
+                        <td class="job-position">
+                            <a href="page-job-details.html">Creative Director</a>
+                        </td>
+                        <td class="job-location">
+                            <div class="job-country">United Kingdom</div>
+                            <div class="job-city">Manchester</div>
+                        </td>
+                        <td class="job-type hidden-phone">PART-TIME</td>
+                    </tr>
+                    <tr>
+                        <td class="job-position">
+                            <a href="page-job-details.html">Interactive Developer</a> <span class="label label-danger">New</span>
+                        </td>
+                        <td class="job-location">
+                            <div class="job-country">United Kingdom</div>
+                            <div class="job-city">Manchester</div>
+                        </td>
+                        <td class="job-type hidden-phone">PART-TIME</td>
+                    </tr>
+                    <tr>
+                        <td class="job-position">
+                            <a href="page-job-details.html">Lead Designer</a>
+                        </td>
+                        <td class="job-location">
+                            <div class="job-country">United Kingdom</div>
+                            <div class="job-city">Manchester</div>
+                        </td>
+                        <td class="job-type hidden-phone">PART-TIME</td>
+                    </tr>
+                    <tr>
+                        <td class="job-position">
+                            <a href="page-job-details.html">Ruby on Rails Developer</a>
+                        </td>
+                        <td class="job-location">
+                            <div class="job-country">United Kingdom</div>
+                            <div class="job-city">Manchester</div>
+                        </td>
+                        <td class="job-type hidden-phone">PART-TIME</td>
+                    </tr>
+                </table>
+            </div>
+            <!-- End Open Vacancies List -->
+
+            <!-- Sidebar -->
+            <div class="col-md-4 col-sm-6">
+                <div class="join-us-promo">
+                    <!-- Quote -->
+                    <div class="join-us-bubble">
+                        <blockquote>
+                            <p class="quote">
+                                "You are very welcome in our team! Ut enim ad minim veniam, quis nostrud exercitation."
+                            </p>
+                            <cite class="author-info">
+                                - Name Surname,<br>Managing Director
+                            </cite>
+                        </blockquote>
+                        <div class="sprite arrow-speech-bubble"></div>
+                    </div>
+                    <!-- Team Member Photo -->
+                    <div class="author-photo">
+                        <img src="img/user2.jpg" alt="Name Surname">
+                    </div>
+                </div>
+            </div>
+            <!-- End Sidebar -->
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-footer col-md-3 col-xs-6">
+                <h3>Our Latest Work</h3>
+                <div class="portfolio-item">
+                    <div class="portfolio-image">
+                        <a href="page-portfolio-item.html"><img src="img/portfolio6.jpg" alt="Project Name"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-footer col-md-3 col-xs-6">
+                <h3>Navigate</h3>
+                <ul class="no-list-style footer-navigate-section">
+                    <li><a href="page-blog-posts.html">Blog</a></li>
+                    <li><a href="page-portfolio-3-columns-2.html">Portfolio</a></li>
+                    <li><a href="page-products-3-columns.html">eShop</a></li>
+                    <li><a href="page-services-3-columns.html">Services</a></li>
+                    <li><a href="page-pricing.html">Pricing</a></li>
+                    <li><a href="page-faq.html">FAQ</a></li>
+                </ul>
+            </div>
+
+            <div class="col-footer col-md-4 col-xs-6">
+                <h3>Contacts</h3>
+                <p class="contact-us-details">
+                    <b>Address:</b> 123 Fake Street, LN1 2ST, London, United Kingdom<br/>
+                    <b>Phone:</b> +44 123 654321<br/>
+                    <b>Fax:</b> +44 123 654321<br/>
+                    <b>Email:</b> <a href="mailto:getintoutch@yourcompanydomain.com">getintoutch@yourcompanydomain.com</a>
+                </p>
+            </div>
+            <div class="col-footer col-md-2 col-xs-6">
+                <h3>Stay Connected</h3>
+                <ul class="footer-stay-connected no-list-style">
+                    <li><a href="#" class="facebook"></a></li>
+                    <li><a href="#" class="twitter"></a></li>
+                    <li><a href="#" class="googleplus"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-copyright">&copy; 2013 mPurpose. All rights reserved.</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Javascripts -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
+<script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
+<script src="js/jquery.fitvids.js"></script>
+<script src="js/jquery.sequence-min.js"></script>
+<script src="js/jquery.bxslider.js"></script>
+<script src="js/main-menu.js"></script>
+<script src="js/template.js"></script>
+
