ab93ff2b0faa9511ff998137114bb032019739c7
sim.i.a <simakovich77@gmail.com>
2016-09-26 16:34:38 +0300

    dfgdfgdfgdfg7

--- public_html/protected/views/user/index.php
+++ public_html/protected/views/user/index.php
@@ -13,17 +13,7 @@
 <div class="section">
 	<div class="container">
 		<div class="row">
-			<!-- Team Member -->
-			<div class="col-md-4 col-sm-6">
-				<div class="team-member">
-					<!-- Team Member Photo -->
-					<div class="team-member-image"><img src="img/team1.jpg" alt="Name Surname"></div>
-					<div class="team-member-info">
-
-					</div>
-				</div>
-			</div>
-			<!-- End Team Member -->
+						<!-- End Team Member -->
 			<div class="col-md-4 col-sm-6">
 				<div class="team-member">
 					<?php
