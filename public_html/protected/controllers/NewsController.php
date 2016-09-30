c8c1a823a1907d6285283959a04377706541adc3
sim.i.a <simakovich77@gmail.com>
2016-09-30 14:51:14 +0300

    news 3

--- public_html/protected/controllers/NewsController.php
+++ public_html/protected/controllers/NewsController.php
@@ -11,10 +11,20 @@ class NewsController extends Controller
         ];
 
         $this->render('index', $dAT);
-        
+
     }
 
+    public function actionNews($id)
+    {
+        $Data = News::model()->findByPk($id);
+        $dAT = [
+            'NewsData'=>$Data,
 
+        ];
+
+        $this->render('news', $dAT);
+
+    }
 
 
 
