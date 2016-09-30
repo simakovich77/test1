7f7c5d39aa8b2329515d18e57fd5cb2ca00c85da
sim.i.a <simakovich77@gmail.com>
2016-09-30 10:19:36 +0300

    slider 11

--- public_html/protected/views/layouts/main.php
+++ public_html/protected/views/layouts/main.php
@@ -24,12 +24,7 @@
 </head>
 <body>
 
-<?php
 
-$this->beginWidget('ext.widgets.SliderBase');
-$this->endWidget();
-
-?>
 
 
 <!--[if lt IE 7]>
@@ -148,37 +143,14 @@ $this->endWidget();
 <div class="homepage-slider">
 	<div id="sequence">
 		<ul class="sequence-canvas">
-			<!-- Slide 1 -->
-			<li class="bg4">
-				<!-- Slide Title -->
-				<h2 class="title">Responsive</h2>
-				<!-- Slide Text -->
-				<h3 class="subtitle">It looks great on desktops, laptops, tablets and smartphones</h3>
-				<!-- Slide Image -->
-				<img class="slide-img" src=<?php echo Yii::app()->request->baseUrl; ?>"/img/homepage-slider/slide1.png" alt="Slide 1" />
-			</li>
-			<!-- End Slide 1 -->
-			<!-- Slide 2 -->
-			<li class="bg3">
-				<!-- Slide Title -->
-				<h2 class="title">Color Schemes</h2>
-				<!-- Slide Text -->
-				<h3 class="subtitle">Comes with 5 color schemes and it's easy to make your own!</h3>
-				<!-- Slide Image -->
-				<img class="slide-img" src=<?php echo Yii::app()->request->baseUrl; ?>"/img/homepage-slider/slide2.png" alt="Slide 2" />
-			</li>
-			<!-- End Slide 2 -->
-			<!-- Slide 3 -->
-			<li class="bg1">
-				<!-- Slide Title -->
-				<h2 class="title">Feature Rich</h2>
-				<!-- Slide Text -->
-				<h3 class="subtitle">Huge amount of components and over 30 sample pages!</h3>
-				<!-- Slide Image -->
-				<img class="slide-img" src=<?php echo Yii::app()->request->baseUrl; ?>"/img/homepage-slider/slide3.png" alt="Slide 3" />
-			</li>
-			<!-- End Slide 3 -->
+			<?php
+
+			$this->beginWidget('ext.widgets.SliderBase');
+			$this->endWidget();
+
+			?>
 		</ul>
+	
 		<div class="sequence-pagination-wrapper">
 			<ul class="sequence-pagination">
 				<li>1</li>
