f8afa42324babec92ea40c1f1af7aee11165d103
simakovich77 <simakovich77@gmail.com>
2016-10-17 13:34:50 +0300

    dfgdg111121

--- public_html/protected/modules/admin/controllers/DefaultController.php
+++ public_html/protected/modules/admin/controllers/DefaultController.php
@@ -17,4 +17,18 @@ class DefaultController extends Controller
 		$this->render('articles', $data);
 
 	}
+
+    public function actionUpdate()
+    {
+
+echo "dfg";
+        $this->render('update');
+    }
+
+    public function actionView()
+    {
+
+        echo "dfg1";
+        $this->render('view');
+    }
 }
