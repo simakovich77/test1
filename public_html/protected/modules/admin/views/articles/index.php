de16846019acc3bad371210e6b902b6691dd2ba6
simakovich77 <simakovich77@gmail.com>
2016-10-17 16:49:20 +0300

    dfgdg111121q21

--- public_html/protected/modules/admin/views/articles/index.php
+++ public_html/protected/modules/admin/views/articles/index.php
@@ -1,19 +1,6 @@
 
 <nav id="mainmenu" class="mainmenu">
-	<ul>
 
-		<li class="active">
-			<a href=<?php echo Yii::app()->request->baseUrl; echo $this->createUrl('/admin/default/articles'); ?>>Статьи</a>
-		</li>
-		<li>
-			<a href="features.html">Features</a>
-		</li>
-		<li class="has-submenu">
-			<a href="#">Pages +</a>
-		<li>
-			<a href="credits.html">Credits</a>
-		</li>
-	</ul>
 </nav>
 
 
--- public_html/protected/modules/admin/views/layouts/column1.php
+++ public_html/protected/modules/admin/views/layouts/column1.php
@@ -11,7 +11,23 @@
 
 </head>
 <body>
-<div class="col-md-4">.col-md-4</div>
+<div class="col-md-4">.col-md-4
+
+    <ul>
+
+        <li class="active">
+            <a href=<?php echo Yii::app()->request->baseUrl; echo $this->createUrl('/admin/default/articles'); ?>>Статьи</a>
+        </li>
+        <li>
+            <a href="features.html">Features</a>
+        </li>
+        <li class="has-submenu">
+            <a href="#">Pages +</a>
+        <li>
+            <a href="credits.html">Credits</a>
+        </li>
+    </ul>
+</div>
 <div class="col-md-8">.col-md-8
     <?php echo $content;?>
 </div>
