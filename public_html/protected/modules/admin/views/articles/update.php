cef9097145270d7e5f738720840746e2fa775de7
simakovich77 <simakovich77@gmail.com>
2016-10-31 21:11:15 +0300

    fgfgfgfg1

--- public_html/protected/modules/admin/views/articles/update.php
+++ public_html/protected/modules/admin/views/articles/update.php
@@ -10,7 +10,7 @@
 
 
 <div class="form-group">
-    <?php echo $form->labelEx($d, 'Заголовок статьи'); ?>
+    <?php echo $form->labelEx($d, 'title'); ?>
     <?php echo $form->textField($d,'title',array('size'=>60,'maxlength'=>255, 'class' => 'form-control')); ?>
     <?php echo $form->error($d,'title'); ?>
 
@@ -22,25 +22,25 @@
 </div>
 
 <div class="form-group">
-    <?php echo $form->labelEx($d, 'Содержимое статьи'); ?>
+    <?php echo $form->labelEx($d, 'text'); ?>
     <?php echo $form->textField($d,'text',array('size'=>255, 'maxlength'=>255, 'class' => 'form-control')); ?>
     <?php echo $form->error($d,'text'); ?>
 </div>
 
 <div class="form-group">
-    <?php echo $form->labelEx($d, 'Фото статьи'); ?>
+    <?php echo $form->labelEx($d, 'img'); ?>
     <?php echo CHtml::image(Yii::app()->request->baseUrl.$d->img,"img",array("width"=>200)); echo "<br>";?>
     <?php echo $form->error($d,'img'); ?>
 </div>
 
 <div class="form-group">
-    <?php echo $form->labelEx($d, 'Выбрать фото статьи'); ?>
-    <?php echo CHtml::activeFileField($d, 'image'); ?>
+    <?php echo $form->labelEx($d, 'img'); ?>
+    <?php echo CHtml::activeFileField($d,'img'); ?>
     <?php echo $form->error($d,'img'); ?>
 </div>
 
 <div class="form-group">
-    <?php echo $form->labelEx($d, 'Дата публикации'); ?>
+    <?php echo $form->labelEx($d, 'ndate'); ?>
     <?php echo $form->textField($d,'ndate',array('size'=>255, 'maxlength'=>255, 'class' => 'form-control')); ?>
     <?php echo $form->error($d,'ndate'); ?>
 </div>
